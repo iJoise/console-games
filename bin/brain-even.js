a391ec16702fdb46d8932952612f5fdb73ec69ab
iJoise <kondratevk.1989@mail.ru>
2021-02-02 23:40:14 +0700

    fix brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -10,35 +10,34 @@ const getRandomInt = (max) => {
    return Math.floor(Math.random() * Math.floor(max));
 };
 
-const commonDivisor = () => {
+const isEven = (num) => num % 2 === 0;
+
+const brainIsEven = () => {
    console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
    let count = 0;
-   for (let i = 0; i < 3; i += 0) {
+   for (let i = 0; i < 3; i += 1) {
       let random = getRandomInt(100);
       console.log(`Question: ${random}`);
-      let oneQuestion = readlineSync.question("Your answer: ");
-      if (random % 2 === 0) {
-         if (oneQuestion === "yes") {
+      let ansver = readlineSync.question("Your answer: ");
+      if (isEven(random)) {
+         if (ansver === "yes") {
             console.log("Correct!");
             count += 1;
          } else {
-            console.log(`'${oneQuestion}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${say.userName}!`);
+            console.log(`'${ansver}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${say.userName}!`);
             break;
          }
       }
-      if (random % 2 !==0) {
-         if (oneQuestion === "no") {
+      if (!isEven(random)) {
+         if (ansver === "no") {
             console.log("Correct!");
             count += 1;
          } else {
-            console.log(`'${oneQuestion}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${say.userName}!`);
+            console.log(`'${ansver}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${say.userName}!`);
             break;
          }
-      }
-      if (count === 3) {
-         console.log(`Congratulations, ${say.userName}!`);
-         break;
       }
    }
+   console.log(`Congratulations, ${say.userName}!`);
 };
-commonDivisor();
+brainIsEven();
